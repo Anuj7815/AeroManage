2ed6ff2e4a1a41266cd9432f7ca3f0ac5b9c8ec7
Anuj7815 <anujgupta08082001@gmail.com>
2025-04-23 17:28:24 +0530

    Update in flight repo

--- src/repositories/flightRepository.js
+++ src/repositories/flightRepository.js
@@ -57,21 +57,30 @@ class FlightRepository extends CrudRepository {
     };
 
     async updateRemainingSeats(flightId, seats, dec = true) {
-        await db.sequelize.query(addRowLockOnFlights(flightId));
-        const flight = await Flight.findByPk(flightId);
-        if (+dec) {
-            await flight.decrement("totalSeats", {
-                by: seats,
-                where: { id: flightId },
-            });
-        } else {
-            await flight.increment("totalSeats", {
-                by: seats,
-                where: { id: flightId },
-            });
+        const transaction = await db.sequelize.transaction();
+        try {
+            console.log("Inside update Remaining seats.");
+            await db.sequelize.query(addRowLockOnFlights(flightId));
+            const flight = await Flight.findByPk(flightId);
+            if (+dec) {
+                await flight.decrement(
+                    "totalSeats",
+                    { by: seats },
+                    { transaction: transaction }
+                );
+            } else {
+                await flight.increment(
+                    "totalSeats",
+                    { by: seats },
+                    { transaction: transaction }
+                );
+            }
+            await transaction.commit();
+            return flight;
+        } catch (error) {
+            await transaction.rollback();
+            throw error;
         }
-        await flight.save();
-        return flight;
     }
 }
 
